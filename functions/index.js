037e9426617d46bf5e4bfd906fac5efb4dc2d64e
Hui Su <yudasuhui@hotmail.com>
2018-06-11 22:46:58 +0200

    horse list
    layout for horse list and filter

--- functions/index.js
+++ functions/index.js
@@ -42,3 +42,90 @@ exports.getSpotList = functions.https.onRequest((request, response) => {
     response.json(spotList);
 });
 
+exports.getHorseList = functions.https.onRequest((request, response) => {
+    const horseList = [
+        {
+            name:'Ascenzione',
+            F:'F: Zack',
+            MF:'MF: Don Schufro',
+            sex:'Mare',
+            age:'5 years',
+            img: "../images/ascenzione.jpg"
+        },
+        {
+            name:'Captain Fleurop',
+            F:'F: Captain Fire',
+            MF:'MF: Fleurop',
+            sex:'Gelding',
+            age:'9 years',
+            img: "../images/captain-fleurop.jpg"
+        },
+        {
+            name:'Chicago',
+            F:'F: Ciacomini',
+            MF:'MF: Carthago',
+            sex:'Gelding',
+            age:'10 years',
+            img: "../images/chicago.jpg"
+        },
+        {
+            name:'Clark',
+            F:'F: Consul',
+            MF:'MF: Come Back II',
+            sex:'Stallion',
+            age:'13 years',
+            img: "../images/clark.jpg"
+        },
+        {
+            name:'Davinci',
+            F:'F: Dancier',
+            MF:'MF: Londontime',
+            sex:'Gelding',
+            age:'5 years',
+            img: "../images/davinci.jpg"
+        },
+        {
+            name:'Djamaica',
+            F:'F: Damon Hill',
+            MF:'MF: Sandro Hit',
+            sex:'Mare',
+            age:'6 years',
+            img: "../images/djamaica.jpg"
+        },
+        {
+            name:'Fascinate',
+            F: 'S: Fürstenball',
+            MF: 'DS: Dacaprio',
+            sex:'Mare',
+            age:'5 years',
+            img: "../images/fascinate.jpg"
+        },
+        {
+            name:'Fellini',
+            F:'F: Ampere',
+            MF:'MF: Jazz',
+            sex:'Gelding',
+            age:'8 years',
+            img: "../images/fellini.jpg"
+        },
+        {
+            name:'Florentina',
+            F:'F: For Romance',
+            MF:'MF: Sandro Hit',
+            sex:'Mare',
+            age:'5 years',
+            img: "../images/florentina.jpg"
+        },
+        {
+            name:'Fraulein Hansemann',
+            F:'F: Floriscount',
+            MF:'MF: Weltmeyer',
+            sex:'Mare',
+            age:'7 years',
+            img: "../images/fraulein-hansemann.jpg"
+        }
+        
+    ];
+
+    response.json(horseList);
+});
--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 $("#select-spot").on("pagebeforeshow", function (event) {
     let spotListView = $('.spot-list');
 
-    if(!spotListView.hasClass('ui-listview')){
+    if (!spotListView.hasClass('ui-listview')) {
         $.ajax({
             url: "getSpotList"
         }).done((data) => {
@@ -24,3 +24,38 @@ $("#select-spot").on("pagebeforeshow", function (event) {
 
 
 });
+
+$("#horse-list").on("pagebeforeshow", function (event) {
+    let horseListView = $('.horse-list');
+
+    if (!horseListView.hasClass('ui-listview')) {
+        $.ajax({
+            url: "getHorseList"
+        }).done((data) => {
+            console.log(data);
+            for (let item of data) {
+                $(`<li class="card ui-grid-a">
+                <div class="ui-block-a">
+                <img class="img-card" src="${item.img}">
+                </div>
+                <div class=" ui-block-b">
+                <h2>${item.name}</h2>
+                <p>${item.F} / ${item.MF}</p>
+                <p>${item.sex}</p>
+                <p>${item.age}</p>
+                <form method="post" action="demoform.asp">
+        <input type="checkbox" data-role="flipswitch" name="switch"  data-on-text="True" data-off-text="False">
+        <br>
+    </form>
+                </div>
+               </li>`).appendTo(horseListView);
+            }
+
+            horseListView.listview();
+            $("input[data-role='flipswitch']").flipswitch();
+        });
+    }
+});
+
+
+
