8b25efc1b8308ab26d73554218f3cdf37a787d2d
Hui Su <yudasuhui@hotmail.com>
2018-06-25 10:24:00 +0200

    submint

--- functions/index.js
+++ functions/index.js
@@ -1,131 +1,143 @@
 const functions = require('firebase-functions');
+const horseList = [
+    {
+        id: 1,
+        name: 'Ascenzione',
+        F: 'F: Zack',
+        MF: 'MF: Don Schufro',
+        sex: 'Mare',
+        age: '5 years',
+        img: "../images/ascenzione.jpg"
+    }, {
+        id: 2,
+        name: 'Captain Fleurop',
+        F: 'F: Captain Fire',
+        MF: 'MF: Fleurop',
+        sex: 'Gelding',
+        age: '9 years',
+        img: "../images/captain-fleurop.jpg"
+    }, {
+        id: 3,
+        name: 'Chicago',
+        F: 'F: Ciacomini',
+        MF: 'MF: Carthago',
+        sex: 'Gelding',
+        age: '10 years',
+        img: "../images/chicago.jpg"
+    }, {
+        id: 4,
+        name: 'Clark',
+        F: 'F: Consul',
+        MF: 'MF: Come Back II',
+        sex: 'Stallion',
+        age: '13 years',
+        img: "../images/clark.jpg"
+    }, {
+        id: 5,
+        name: 'Davinci',
+        F: 'F: Dancier',
+        MF: 'MF: Londontime',
+        sex: 'Gelding',
+        age: '5 years',
+        img: "../images/davinci.jpg"
+    }, {
+        id: 6,
+        name: 'Djamaica',
+        F: 'F: Damon Hill',
+        MF: 'MF: Sandro Hit',
+        sex: 'Mare',
+        age: '6 years',
+        img: "../images/djamaica.jpg"
+    }, {
+        id: 7,
+        name: 'Fascinate',
+        F: 'S: Fürstenball',
+        MF: 'DS: Dacaprio',
+        sex: 'Mare',
+        age: '5 years',
+        img: "../images/fascinate.jpg"
+    }, {
+        id: 8,
+        name: 'Fellini',
+        F: 'F: Ampere',
+        MF: 'MF: Jazz',
+        sex: 'Gelding',
+        age: '8 years',
+        img: "../images/fellini.jpg"
+    }, {
+        id: 9,
+        name: 'Florentina',
+        F: 'F: For Romance',
+        MF: 'MF: Sandro Hit',
+        sex: 'Mare',
+        age: '5 years',
+        img: "../images/florentina.jpg"
+    }, {
+        id: 10,
+        name: 'Fraulein Hansemann',
+        F: 'F: Floriscount',
+        MF: 'MF: Weltmeyer',
+        sex: 'Mare',
+        age: '7 years',
+        img: "../images/fraulein-hansemann.jpg"
+    }
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+];
 
-exports.getSpotList = functions.https.onRequest((request, response) => {
-    const spotList = [
-        {
-            id: 'car',
-            title: "Cars",
-            subtitle: "Find your favorite ride",
-            img: "../images/car.png"
-        },
-        {
-            id: 'house',
-            title: "Real Estate",
-            subtitle: "What about a new life in bigger house",
-            img: "../images/house.png"
-        },
-        {
-            id: 'bike',
-            title: "Bikes",
-            subtitle: "Get a flexible way to roll out",
-            img: "../images/bike.png"
-        },
+// // Create and Deploy Your First Cloud Functions //
+// https://firebase.google.com/docs/functions/write-firebase-functions
 
-        {
-            id: 'travel',
-            title: "Travels",
-            subtitle: "Have a great trip and souvenirs",
-            img: "../images/suitcase.png"
-        },
+exports.getSpotList = functions
+    .https
+    .onRequest((request, response) => {
+        const spotList = [
+            {
+                id: 'car',
+                title: "Cars",
+                subtitle: "Find your favorite ride",
+                img: "../images/car.png"
+            }, {
+                id: 'house',
+                title: "Real Estate",
+                subtitle: "What about a new life in bigger house",
+                img: "../images/house.png"
+            }, {
+                id: 'bike',
+                title: "Bikes",
+                subtitle: "Get a flexible way to roll out",
+                img: "../images/bike.png"
+            }, {
+                id: 'travel',
+                title: "Travels",
+                subtitle: "Have a great trip and souvenirs",
+                img: "../images/suitcase.png"
+            }, {
+                id: 'motor',
+                title: "Motor Bikes",
+                subtitle: "Pretty cool, isn't it",
+                img: "../images/motor.png"
+            }
+        ];
 
-        {
-            id: 'motor',
-            title: "Motor Bikes",
-            subtitle: "Pretty cool, isn't it",
-            img: "../images/motor.png"
-        }
-    ];
+        response.json(spotList);
+    });
 
-    response.json(spotList);
-});
+exports.getHorseList = functions
+    .https
+    .onRequest((request, response) => {
+        response.json(horseList);
+    });
 
-exports.getHorseList = functions.https.onRequest((request, response) => {
-    const horseList = [
-        {
-            name:'Ascenzione',
-            F:'F: Zack',
-            MF:'MF: Don Schufro',
-            sex:'Mare',
-            age:'5 years',
-            img: "../images/ascenzione.jpg"
-        },
-        {
-            name:'Captain Fleurop',
-            F:'F: Captain Fire',
-            MF:'MF: Fleurop',
-            sex:'Gelding',
-            age:'9 years',
-            img: "../images/captain-fleurop.jpg"
-        },
-        {
-            name:'Chicago',
-            F:'F: Ciacomini',
-            MF:'MF: Carthago',
-            sex:'Gelding',
-            age:'10 years',
-            img: "../images/chicago.jpg"
-        },
-        {
-            name:'Clark',
-            F:'F: Consul',
-            MF:'MF: Come Back II',
-            sex:'Stallion',
-            age:'13 years',
-            img: "../images/clark.jpg"
-        },
-        {
-            name:'Davinci',
-            F:'F: Dancier',
-            MF:'MF: Londontime',
-            sex:'Gelding',
-            age:'5 years',
-            img: "../images/davinci.jpg"
-        },
-        {
-            name:'Djamaica',
-            F:'F: Damon Hill',
-            MF:'MF: Sandro Hit',
-            sex:'Mare',
-            age:'6 years',
-            img: "../images/djamaica.jpg"
-        },
-        {
-            name:'Fascinate',
-            F: 'S: Fürstenball',
-            MF: 'DS: Dacaprio',
-            sex:'Mare',
-            age:'5 years',
-            img: "../images/fascinate.jpg"
-        },
-        {
-            name:'Fellini',
-            F:'F: Ampere',
-            MF:'MF: Jazz',
-            sex:'Gelding',
-            age:'8 years',
-            img: "../images/fellini.jpg"
-        },
-        {
-            name:'Florentina',
-            F:'F: For Romance',
-            MF:'MF: Sandro Hit',
-            sex:'Mare',
-            age:'5 years',
-            img: "../images/florentina.jpg"
-        },
-        {
-            name:'Fraulein Hansemann',
-            F:'F: Floriscount',
-            MF:'MF: Weltmeyer',
-            sex:'Mare',
-            age:'7 years',
-            img: "../images/fraulein-hansemann.jpg"
-        }
-        
-    ];
+exports.getOneHorse = functions.https.onRequest((request, response)=>{
+    response.json([{
+        id: 10,
+        name: 'Fraulein Hansemann',
+        F: 'F: Floriscount',
+        MF: 'MF: Weltmeyer',
+        sex: 'Mare',
+        age: '7 years',
+        img: "../images/fraulein-hansemann.jpg"
+    }])
+})
 
-    response.json(horseList);
-});
+    
--- public/js/app.js
+++ public/js/app.js
@@ -27,6 +27,8 @@ $("#select-spot").on("pagebeforeshow", function (event) {
 
 $("#horse-list").on("pagebeforeshow", function (event) {
     let horseListView = $('.horse-list');
+    let pedigreeF = $('#pedigree-F');
+    let pedigreeMF = $('#pedigree-MF');
 
     if (!horseListView.hasClass('ui-listview')) {
         $.ajax({
@@ -34,28 +36,70 @@ $("#horse-list").on("pagebeforeshow", function (event) {
         }).done((data) => {
             console.log(data);
             for (let item of data) {
-                $(`<li class="card ui-grid-a">
+                $(`<li class="card oneHorse ui-grid-a" href="#rating?horse=${item.id}">
                 <div class="ui-block-a">
                 <img class="img-card" src="${item.img}">
                 </div>
                 <div class=" ui-block-b">
-                <h2>${item.name}</h2>
+                <h2>
+                <span>${item.name}</span>
+                <span class="rating"> 5.0 </span>
+                </h2>
                 <p>${item.F} / ${item.MF}</p>
                 <p>${item.sex}</p>
                 <p>${item.age}</p>
-                <form method="post" action="demoform.asp">
-        <input type="checkbox" data-role="flipswitch" name="switch"  data-on-text="True" data-off-text="False">
-        <br>
-    </form>
+               
                 </div>
                </li>`).appendTo(horseListView);
             }
-
+            for (let item of data) {
+                $(`<option>${item.F}</option>`).appendTo(pedigreeF);
+                $(`<option>${item.MF}</option>`).appendTo(pedigreeMF);
+            }
             horseListView.listview();
-            $("input[data-role='flipswitch']").flipswitch();
+            //$("input[data-role='flipswitch']").flipswitch();
+            pedigreeF.selectmenu();
+            pedigreeMF.selectmenu();
+            $(".oneHorse").bind("tap", tapHandler);
+
+            function tapHandler(event) {
+                $.mobile.navigate( $(this).attr( "href" ));
+              
+            }
         });
     }
 });
+
+$("#rating").on("pagebeforeshow", function (event) {
+    let main=$("#rating-page");
+    $.ajax({
+        url: "getOneHorse"
+    }).done((data) => {
+        var item=data[0];
+        var rating="";
+        console.log(item);
+        //console.log(data[0])
+        $(`<img src="${item.img}">
+        <h2>
+                <span>${item.name}</span>
+                <span class="rating"> ${rating} </span>
+                </h2>
+                <p>${item.F} / ${item.MF}</p>
+                <p>${item.sex}</p>
+                <p>${item.age}</p>
+                <hr>
+                
+                `).appendTo(main);
+                
+        
+        //$(".slider").slider();
+    })
+
+    
+
+});
+
+
 
 
 
