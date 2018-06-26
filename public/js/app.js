3c02d20c841995063b3a19b60b146b9173b586d3
Yudatech <yudasuhui@hotmail.com>
2018-06-26 23:53:30 +0200

    v0.1 demo version

--- public/js/app.js
+++ public/js/app.js
@@ -9,10 +9,23 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var currentSpot = {
+    id: "F1XtT9ll8GlT98hCanNZ",
+    F: "Dancier",
+    MF: "Londontime",
+    age: "5 years",
+    img: "../images/davinci.jpg",
+    name: "Davinci",
+    sex: "Gelding"
+};
+
+var currentCodeList = "m6g018XblXf7duo9ghkj";
+
 // Initialize Cloud Firestore through Firebase
 var firestore = firebase.firestore();
 const settings = {/* your settings... */ timestampsInSnapshots: true};
 firestore.settings(settings);
+
 
 $("#select-spot").on("pagebeforeshow", function (event) {
     let spotListView = $('.spot-list');
@@ -47,73 +60,365 @@ $("#horse-list").on("pagebeforeshow", function (event) {
     let pedigreeMF = $('#pedigree-MF');
 
     if (!horseListView.hasClass('ui-listview')) {
-        loadingMask('#horse-list .ui-content', true);
+        loadingMask(true, '#horse-list .ui-content');
 
-        firestore.collection("horses").get().then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                let item = doc.data();
+        firestore.collection("codelists").doc(currentCodeList).get().then((codeList) => {
+            let horses = codeList.data().horses;
+            let count = 0;
 
-                $(`<li class="card ui-grid-a">
-                <div class="ui-block-a">
-                <img class="img-card" src="${item.img}">
-                </div>
-                <div class=" ui-block-b">
-                <h2>
-                <span>${item.name}</span>
-                <span class="rating"> 5.0 </span>
-                </h2>
-                <p>${item.F} / ${item.MF}</p>
-                <p>${item.sex}</p>
-                <p>${item.age}</p>
-                <form method="post" action="demoform.asp">
-                    <input type="checkbox" data-role="flipswitch" name="switch"  data-on-text="True" data-off-text="False">
-                    <br>
-                </form>
-                </div>
-               </li>`).appendTo(horseListView);
-            });
+            for (let horse of horses) {
+                firestore.collection("horses").doc(horse).get().then((querySnapshot) => {
 
-            horseListView.listview();
-            $("input[data-role='flipswitch']").flipswitch();
+                    let item = querySnapshot.data();
 
-            loadingMask('#horse-list .ui-content', false);
+                    let el = $(`<li class="card ui-grid-a" data-id="${horse}">
+                                    <div class="ui-block-a">
+                                        <img class="img-card" src="${item.img}">
+                                    </div>
+                                    <div class=" ui-block-b">
+                                        <h2>
+                                            <span>${item.name}</span>
+                                        </h2>
+                                        <p>${item.F} / ${item.MF}</p>
+                                        <p>${item.sex}</p>
+                                        <p>${item.age}</p>
+                                    </div>
+                               </li>`);
+
+                    el.appendTo(horseListView);
+
+                    el.bind("tap", (event) => {
+                        currentSpot = item;
+                        currentSpot.id = horse;
+
+                        $.mobile.navigate("#rating");
+                    });
+
+                    let rate;
+                    firestore.collection("ratings").where("spot_id", "==", horse).get().then((querySnapshot) => {
+                        if (querySnapshot.docs.length > 0) {
+                            rate = calcAvgRating(querySnapshot.docs[0].data());
+                        }
+
+                        if (rate) {
+                            el.append(`<div class="ribbon ribbon-top-right"><span>${rate}</span></div>`);
+                        }
+                    });
+
+                    count++;
+
+                    if (count >= horses.length) {
+                        horseListView.listview();
+                        loadingMask(false);
+                    }
+                });
+            }
         });
-
-        // $.ajax({
-        //     url: "getHorseList"
-        // }).done((data) => {
-        //     console.log(data);
-        //     for (let item of data) {
-        //         $(`<li class="card ui-grid-a">
-        //         <div class="ui-block-a">
-        //         <img class="img-card" src="${item.img}">
-        //         </div>
-        //         <div class=" ui-block-b">
-        //         <h2>${item.name}</h2>
-        //         <p>${item.F} / ${item.MF}</p>
-        //         <p>${item.sex}</p>
-        //         <p>${item.age}</p>
-        //         <form method="post" action="demoform.asp">
-        //             <input type="checkbox" data-role="flipswitch" name="switch"  data-on-text="True" data-off-text="False">
-        //             <br>
-        //         </form>
-        //         </div>
-        //        </li>`).appendTo(horseListView);
-        //     }
-        //
-        //     horseListView.listview();
-        //     $("input[data-role='flipswitch']").flipswitch();
-        // });
     }
 });
 
-function loadingMask(container, switcher){
-    if(switcher){
+$("#rating").on("pagebeforeshow", function (event) {
+    let infoPanel = $('#rating .spot-info');
+
+    loadingMask(true, '#rating .ui-content');
+
+    firestore.collection("ratings").where("spot_id", "==", currentSpot.id).get().then((querySnapshot) => {
+
+        if (querySnapshot.docs.length > 0) {
+            let rating = querySnapshot.docs[0].data();
+
+            applyRatings(rating);
+
+            loadingMask(false);
+        }
+
+        else {
+            applyRatings({});
+
+            loadingMask(false);
+        }
+
+
+        $('#rating .slider').off().change((event) => {
+            let rate = calcAvgRating(getRatings());
+
+            if (rate) {
+                $("#rating .spot-info-rate").text(rate);
+            } else {
+                $("#rating .spot-info-rate").text("-.-");
+            }
+        });
+
+        $("#rating .ui-content").scrollTop(0);
+    });
+
+    if (infoPanel.length > 0) {
+        infoPanel.find('.spot-info-name').text(currentSpot.name);
+        infoPanel.find('.spot-info-img').attr('src', currentSpot.img);
+        infoPanel.find('.spot-info-F').text(currentSpot.F);
+        infoPanel.find('.spot-info-MF').text(currentSpot.MF);
+        infoPanel.find('.spot-info-sex').text(currentSpot.sex);
+        infoPanel.find('.spot-info-age').text(currentSpot.age);
+    }
+});
+
+$("#rating .save-btn").bind("tap", function (event) {
+    let rating = getRatings();
+
+    loadingMask(true, '#rating .ui-content');
+
+    firestore.collection("ratings").where("spot_id", "==", currentSpot.id).get().then((querySnapshot) => {
+        let docId;
+
+        if (querySnapshot.docs.length > 0) {
+            docId = querySnapshot.docs[0].id;
+
+            firestore.collection("ratings").doc(docId).set(rating).then(() => {
+                loadingMask(false);
+                $.mobile.navigate("#horse-list");
+            }).catch((error) => {
+                console.error("Error writing document: ", error);
+            });
+        } else {
+            firestore.collection("ratings").add(rating).then(() => {
+                loadingMask(false);
+                $.mobile.navigate("#horse-list");
+            }).catch((error) => {
+                console.error("Error writing document: ", error);
+            });
+        }
+
+        let rate = calcAvgRating(rating);
+        let card = $("#horse-list .card[data-id=" + currentSpot.id + "]");
+        card.find('.ribbon').remove();
+        card.append(`<div class="ribbon ribbon-top-right"><span>${rate}</span></div>`);
+
+        card = $("#personal-list .card[data-id=" + currentSpot.id + "]");
+        card.find('.ribbon').remove();
+        card.append(`<div class="ribbon ribbon-top-right"><span>${rate}</span></div>`);
+    });
+});
+
+$("#rating .cancel-btn").bind("tap", (event) => {
+    $.mobile.navigate("#horse-list");
+});
+
+$("#get-code .fetch-code-btn").bind("tap", (event) => {
+    let code = $("#list-code").val();
+    let error = $(".not-found-message");
+
+    loadingMask(true, 'body');
+
+    firestore.collection("codelists").where("code", "==", code).get().then((codelists) => {
+        if (codelists.docs.length > 0) {
+            currentCodeList = codelists.docs[0].id;
+            $("#horse-list .horse-list").remove();
+            $("#horse-list .ui-content").append("<ul class='horse-list' data-filter='true'></ul>");
+
+            $.mobile.navigate("#horse-list");
+
+            error.hide();
+            $("#list-code").val("");
+        } else {
+            loadingMask(false);
+            error.show();
+        }
+    });
+});
+
+
+$("#personal-list").on("pagebeforeshow", function (event) {
+    let personalListView = $('.personal-list');
+
+    if (!personalListView.hasClass('ui-listview')) {
+        loadingMask(true, '#personal-list .ui-content');
+
+        firestore.collection("ratings").get().then((ratings) => {
+            var count = 0;
+
+            ratings.forEach((doc) => {
+                let rate = doc.data();
+
+                //firestore.collection("horses").where("id", "==", rate.spot_id).get().then((horse) => {
+                firestore.collection("horses").doc(rate.spot_id).get().then((horse) => {
+                    let item = horse.data();
+
+                    let el = $(`<li class="card ui-grid-a" data-id="${rate.spot_id}">
+                                    <div class="ui-block-a">
+                                        <img class="img-card" src="${item.img}">
+                                    </div>
+                                    <div class=" ui-block-b">
+                                        <h2>
+                                            <span>${item.name}</span>
+                                        </h2>
+                                        <p>${item.F} / ${item.MF}</p>
+                                        <p>${item.sex}</p>
+                                        <p>${item.age}</p>
+                                    </div>
+                               </li>`);
+
+                    el.appendTo(personalListView);
+
+                    el.bind("tap", (event) => {
+                        currentSpot = item;
+                        currentSpot.id = rate.spot_id;
+
+                        $.mobile.navigate("#rating");
+                    });
+
+                    el.append(`<div class="ribbon ribbon-top-right"><span>${calcAvgRating(rate)}</span></div>`);
+
+
+                    count++;
+                    if (count >= ratings.docs.length) {
+                        personalListView.listview();
+                        loadingMask(false);
+                    }
+                });
+            });
+        });
+
+    }
+});
+
+$("#compare-list").on("pagebeforeshow", function (event) {
+    let compareTable = $('.compare-table-tbody');
+
+    loadingMask(true, '#compare-list .ui-content');
+
+    firestore.collection("ratings").get().then((ratings) => {
+        var count = 0;
+
+        ratings.forEach((doc) => {
+            let rate = doc.data();
+
+            firestore.collection("horses").doc(rate.spot_id).get().then((horse) => {
+                let item = horse.data();
+
+                let el = $(`<tr>
+                                <td class="td-name">${item.name}</td>
+                                <td class="td-total">${calcAvgRating(rate)}</td>  
+                                <td>${rate.sex}</td>  
+                                <td>${rate.age}</td>  
+                                <td>${rate.pedigree}</td>  
+                                <td>${rate.color}</td>  
+                                <td>${rate.height}</td>  
+                                <td>${rate.rideability}</td>  
+                                <td>${rate.walk}</td>  
+                                <td>${rate.trot}</td>  
+                                <td>${rate.canter}</td>  
+                             </tr>`);
+
+                el.appendTo(compareTable);
+
+                count++;
+                if (count >= ratings.docs.length) {
+                    $("#table-column-toggle").table("refresh");
+                    let totals = $("#table-column-toggle td.td-total");
+
+                    totals.each((index)=>{
+                        let cell = totals[index];
+                        let val = $(cell).text()/1;
+                        if(val>4) $(cell).addClass('best');
+                        else if(val>2) $(cell).addClass('better');
+                        else $(cell).addClass('good');
+                    });
+
+                    $("#table-column-toggle").table("refresh");
+                    let subs = $("#table-column-toggle td:not(.td-total):not(.td-name)");
+
+                    subs.each((index)=>{
+                        let cell = subs[index];
+                        let val = $(cell).text()/1;
+                        if(val>4) $(cell).addClass('best-sub');
+                        else if(val>2) $(cell).addClass('better-sub');
+                        else $(cell).addClass('good-sub');
+                    });
+
+                    loadingMask(false);
+                }
+            });
+        });
+    });
+});
+
+function loadingMask(switcher, container) {
+    if (switcher) {
         $(container).append('<div class="dzidr-mask"><div class="loader"></div></div>');
     }
 
     else {
-        $('.dzidr-mask').remove();
+        $('.dzidr-mask').fadeOut(500, () => {
+            $('.dzidr-mask').remove()
+        });
     }
 }
 
+function applyRatings(rating) {
+    $("#rating-sex").val(rating.sex / 1);
+    $("#rating-age").val(rating.age / 1);
+    $("#rating-pedigree").val(rating.pedigree / 1);
+    $("#rating-color").val(rating.color / 1);
+    $("#rating-height").val(rating.height / 1);
+    $("#rating-rideability").val(rating.rideability / 1);
+    $("#rating-walk").val(rating.walk / 1);
+    $("#rating-trot").val(rating.trot / 1);
+    $("#rating-canter").val(rating.canter / 1);
+
+    $('.spot-rating .slider').slider('refresh');
+
+    let rate = calcAvgRating(rating);
+    if (rate) {
+        $("#rating .spot-info-rate").text(rate);
+    } else {
+        $("#rating .spot-info-rate").text("-.-");
+    }
+}
+
+function getRatings() {
+    let sex = $("#rating-sex").val(),
+        age = $("#rating-age").val(),
+        pedigree = $("#rating-pedigree").val(),
+        color = $("#rating-color").val(),
+        height = $("#rating-height").val(),
+        rideability = $("#rating-rideability").val(),
+        walk = $("#rating-walk").val(),
+        trot = $("#rating-trot").val(),
+        canter = $("#rating-canter").val(),
+        rating = {
+            sex: sex,
+            age: age,
+            pedigree: pedigree,
+            color: color,
+            height: height,
+            rideability: rideability,
+            walk: walk,
+            trot: trot,
+            canter: canter,
+            spot_id: currentSpot.id
+        };
+
+    return rating;
+
+}
+
+function calcAvgRating(rating) {
+    if (!rating) return null;
+
+    let sum = 0,
+        count = 0;
+
+    for (let r in rating) {
+        if (rating[r] && r != 'spot_id') {
+            let add = rating[r] / 1;
+
+            if (add !== 0) {
+                sum += add;
+                count++;
+            }
+        }
+    }
+
+    if (count === 0) return null;
+    return (sum / count).toFixed(1);
+}
--- public/js/initDb.js
+++ public/js/initDb.js
@@ -6,80 +6,80 @@ db.settings(settings);
 var horseList = [
     {
         name: 'Ascenzione',
-        F: 'F: Zack',
-        MF: 'MF: Don Schufro',
+        F: 'Zack',
+        MF: 'Don Schufro',
         sex: 'Mare',
         age: '5 years',
         img: "../images/ascenzione.jpg"
     },
     {
         name: 'Captain Fleurop',
-        F: 'F: Captain Fire',
-        MF: 'MF: Fleurop',
+        F: 'Captain Fire',
+        MF: 'Fleurop',
         sex: 'Gelding',
         age: '9 years',
         img: "../images/captain-fleurop.jpg"
     },
     {
         name: 'Chicago',
-        F: 'F: Ciacomini',
-        MF: 'MF: Carthago',
+        F: 'Ciacomini',
+        MF: 'Carthago',
         sex: 'Gelding',
         age: '10 years',
         img: "../images/chicago.jpg"
     },
     {
         name: 'Clark',
-        F: 'F: Consul',
-        MF: 'MF: Come Back II',
+        F: 'Consul',
+        MF: 'Come Back II',
         sex: 'Stallion',
         age: '13 years',
         img: "../images/clark.jpg"
     },
     {
         name: 'Davinci',
-        F: 'F: Dancier',
-        MF: 'MF: Londontime',
+        F: 'Dancier',
+        MF: 'Londontime',
         sex: 'Gelding',
         age: '5 years',
         img: "../images/davinci.jpg"
     },
     {
         name: 'Djamaica',
-        F: 'F: Damon Hill',
-        MF: 'MF: Sandro Hit',
+        F: 'Damon Hill',
+        MF: 'Sandro Hit',
         sex: 'Mare',
         age: '6 years',
         img: "../images/djamaica.jpg"
     },
     {
         name: 'Fascinate',
-        F: 'S: Fürstenball',
-        MF: 'DS: Dacaprio',
+        F: 'Fürstenball',
+        MF: 'Dacaprio',
         sex: 'Mare',
         age: '5 years',
         img: "../images/fascinate.jpg"
     },
     {
         name: 'Fellini',
-        F: 'F: Ampere',
-        MF: 'MF: Jazz',
+        F: 'Ampere',
+        MF: 'Jazz',
         sex: 'Gelding',
         age: '8 years',
         img: "../images/fellini.jpg"
     },
     {
         name: 'Florentina',
-        F: 'F: For Romance',
-        MF: 'MF: Sandro Hit',
+        F: 'For Romance',
+        MF: 'Sandro Hit',
         sex: 'Mare',
         age: '5 years',
         img: "../images/florentina.jpg"
     },
     {
         name: 'Fraulein Hansemann',
-        F: 'F: Floriscount',
-        MF: 'MF: Weltmeyer',
+        F: 'Floriscount',
+        MF: 'Weltmeyer',
         sex: 'Mare',
         age: '7 years',
         img: "../images/fraulein-hansemann.jpg"
